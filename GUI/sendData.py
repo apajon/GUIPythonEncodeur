8f8e323bc7383d6414456a39f9c0dd9d76460155
Dominic Letourneau <dominic.letourneau@usherbrooke.ca>
2021-01-10 19:38:30 -0500

    Send data now works

--- GUI/sendData.py
+++ GUI/sendData.py
@@ -1,9 +1,12 @@
 import time
 import json
-import pandas as pd
 import numpy as np
-import configparser as ConfigParser  # Python 3
 from lib_global_python import MQTT_client
+try:
+    import ConfigParser #Python 2
+except ImportError:
+    import configparser as ConfigParser #Python 3
+
 
 config = ConfigParser.ConfigParser()
 config.read('config.cfg')
@@ -11,15 +14,19 @@ client = MQTT_client.createClient("Encoder", config)
 dataFromFile = np.genfromtxt("Logger_encoder_gel_1cm_v1_00.txt", delimiter=",", names=True)
 clientTopic = config.get('MQTT', 'topic_publish')
 #print(data)
-print(dataFromFile["PositionChange"])
-poitionChange=dataFromFile["PositionChange"]
-data = {
-    "TimeRecording": t1,
-    "PositionChange": positionChange,
-    "TimeChange": timeChange,
-    "IndexTriggered": indexTriggered
-}
-json_string = json.dumps(data)
+t1=dataFromFile["TimeRecording"]
+positionChange=dataFromFile["PositionChange"]
+timeChange=dataFromFile["TimeChange"]
+indexTriggered=dataFromFile["IndexTriggered"]
+      
+for i in range(0, len(dataFromFile["TimeRecording"]),1):
+    data=  {
+    "TimeRecording": t1[i],
+    "PositionChange": positionChange[i],
+    "TimeChange": timeChange[i],
+    "IndexTriggered": indexTriggered[i]
+    }
+    json_string = json.dumps(data)
+    client.publish(clientTopic,json.dumps(data))
 
-#publish 'data' in topic as a JSON string
-client.publish(clientTopic,json.dumps(data))
+
