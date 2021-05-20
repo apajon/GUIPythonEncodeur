a6018d7fb048bcf4d1cde7a535d298e128efcc72
Adrien Pajon <adrien.pajon@gmail.com>
2021-05-20 12:32:50 -0400

    minor modif

--- GUI/Encoder_Control_GUI_ONLY_modif_RP+graph.py
+++ GUI/Encoder_Control_GUI_ONLY_modif_RP+graph.py
@@ -19,15 +19,7 @@ from matplotlib.figure import Figure
 import random
 
 from lib import configFile
-
-
-class MplCanvas(FigureCanvasQTAgg):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
-
+from lib.MplCanvas import MplCanvas
 
 class Ui_Style(Ui_Tester):
 
